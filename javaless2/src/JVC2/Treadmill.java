248e4640a04a63cbd4566479727d3690fb5a5a0d
Zhanay <zhanayadassov@gmail.com>
2021-12-22 21:39:54 +0600

    less1

---
+++ javaless2/src/JVC2/Treadmill.java
@@ -0,0 +1,15 @@
+package JVC2;
+
+public class Treadmill {
+    private int range;
+
+    public Treadmill(int range) {
+        this.range = range;
+    }
+
+    public int getRange() {
+        return range;
+    }
+
+    public void setRange(int range) { this.range = range; }
+}
---
+++ javaless2/src/JVC2/Wall.java
@@ -0,0 +1,13 @@
+package JVC2;
+
+public class Wall {
+    private int Height;
+
+    public Wall(int height) {
+        this.Height = height;
+    }
+
+    public int getHeight() { return Height; }
+
+    public void setHeight(int height) { Height = height; }
+}
